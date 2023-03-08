d1d3d4e082c1bc7bdc0356afa8bc91d4e9a67a22
Marcinek <marcinbator.ofc@gmail.com>
2023-03-08 21:25:44 +0100

    refactor js

--- src/v_full/script.js
+++ src/v_full/script.js
@@ -1,17 +1,15 @@
+//Buttons
 const revertButton = document.getElementById("flt-revert");
 const leftButton = document.getElementById("flt-left");
 const rightButton = document.getElementById("flt-right");
 const tableButton = document.getElementById("flt-table-button");
-const table = document.getElementById("flt-table");
-/////////
-const menu1 = document.getElementById("flt-menu-1");
-const menu2 = document.getElementById("flt-menu-2");
-const menu3 = document.getElementById("flt-menu-3");
-const menu4 = document.getElementById("flt-menu-4");
-////////
+const menuElements = document.querySelectorAll(".flt-side-elem");
+//Elements
 const slide = document.getElementById("flt-slide");
 const sectionTitle = document.getElementById("flt-section-title");
-////////
+const table = document.getElementById("flt-table");
+const tableElements = document.querySelectorAll(".flt-table-elem");
+//Arrays
 const slides = [
   //1
   "<h2>Slajd 1</h2>",
@@ -36,154 +34,63 @@ const sections = [
   "Domieszkowanie lokalne",
   "Nowinki techniczne",
 ];
-const tableElements = document.querySelectorAll(".flt-table-elem");
-console.log(tableElements);
 const sectionNums = [0, 3, 6, 9];
-////////
+//Indicators
 var currentSlide = 0;
 var currentSection = 1;
+//Functions
 function changeSlide(num) {
   slide.innerHTML = slides[num];
   currentSlide = num;
+  checkArrows();
+  checkSections();
+}
+function checkArrows() {
   if (currentSlide > 0) {
+    leftButton.classList.remove("flt-none");
     revertButton.classList.remove("flt-none");
   } else {
+    leftButton.classList.add("flt-none");
     revertButton.classList.add("flt-none");
   }
-  console.log("S " + currentSection);
-  console.log("Sl " + currentSlide);
-}
-function changeSlide2(num) {
-  slide.innerHTML = slides[num];
-  currentSlide = num;
-  if (currentSlide > 0) {
-    revertButton.classList.remove("flt-none");
+  if (currentSlide == slides.length - 1) {
+    rightButton.classList.add("flt-none");
   } else {
-    revertButton.classList.add("flt-none");
-  }
-  if (num >= sectionNums[0] && num < sectionNums[1]) {
-    document
-      .getElementById("flt-menu-" + currentSection)
-      .classList.remove("flt-menu-active");
-    document.getElementById("flt-menu-" + 1).classList.add("flt-menu-active");
-    currentSection = 1;
-  }
-  if (num >= sectionNums[1] && num < sectionNums[2]) {
-    document
-      .getElementById("flt-menu-" + currentSection)
-      .classList.remove("flt-menu-active");
-    document.getElementById("flt-menu-" + 2).classList.add("flt-menu-active");
-    currentSection = 2;
-  }
-  if (num >= sectionNums[2] && num < sectionNums[3]) {
-    document
-      .getElementById("flt-menu-" + currentSection)
-      .classList.remove("flt-menu-active");
-    document.getElementById("flt-menu-" + 3).classList.add("flt-menu-active");
-    currentSection = 3;
-  }
-  if (num >= sectionNums[3] && num < sectionNums[4]) {
-    document
-      .getElementById("flt-menu-" + currentSection)
-      .classList.remove("flt-menu-active");
-    document.getElementById("flt-menu-" + 4).classList.add("flt-menu-active");
-    currentSection = 4;
+    rightButton.classList.remove("flt-none");
   }
 }
-function toggleSection(num) {
-  document
-    .getElementById("flt-menu-" + currentSection)
-    .classList.remove("flt-menu-active");
-  document.getElementById("flt-menu-" + num).classList.add("flt-menu-active");
-  currentSection = num;
-  currentSlide = sectionNums[num - 1];
-  sectionTitle.textContent = sections[currentSection - 1];
-  changeSlide(currentSlide);
-  if (currentSlide > 0) {
-    leftButton.classList.remove("flt-none");
+function checkSections() {
+  for (var i = 0; i < 4; i++) {
+    var a = i + 1;
+    if (currentSlide >= sectionNums[i] && currentSlide < sectionNums[a]) {
+      document
+        .getElementById("flt-menu-" + currentSection)
+        .classList.remove("flt-menu-active");
+      document.getElementById("flt-menu-" + a).classList.add("flt-menu-active");
+      currentSection = a;
+    }
   }
-  if (currentSlide == 0) {
-    leftButton.classList.add("flt-none");
-  }
-  if (currentSlide == slides.length) {
-    rightButton.classList.add("flt-none");
-  }
-  if (currentSlide < slides.length) {
-    rightButton.classList.remove("flt-none");
-  }
-  console.log("S " + currentSection);
-  console.log("Sl " + currentSlide);
 }
-////////
-menu1.addEventListener("click", function () {
-  toggleSection(1);
-});
-menu2.addEventListener("click", function () {
-  toggleSection(2);
-});
-menu3.addEventListener("click", function () {
-  toggleSection(3);
-});
-menu4.addEventListener("click", function () {
-  toggleSection(4);
-});
-revertButton.addEventListener("click", function () {
-  changeSlide(0, menu1.textContent);
-  toggleSection(1);
-  rightButton.classList.remove("flt-none");
-  leftButton.classList.add("flt-none");
-});
-rightButton.addEventListener("click", function () {
-  if (currentSlide + 2 < slides.length) {
-    rightButton.classList.remove("flt-none");
-    leftButton.classList.remove("flt-none");
-    changeSlide(currentSlide + 1);
-  } else if (currentSlide + 2 == slides.length) {
-    rightButton.classList.add("flt-none");
-    changeSlide(currentSlide + 1);
-  }
-  if (currentSlide == sectionNums[currentSection]) {
-    toggleSection(currentSection + 1);
-  }
-});
-leftButton.addEventListener("click", function () {
-  if (currentSlide - 1 > 0) {
-    leftButton.classList.remove("flt-none");
-    changeSlide(currentSlide - 1);
-    rightButton.classList.remove("flt-none");
-  } else if (currentSlide - 1 == 0) {
-    leftButton.classList.add("flt-none");
-    changeSlide(currentSlide - 1);
-  }
-  if (currentSlide + 1 == sectionNums[currentSection - 1]) {
-    document
-      .getElementById("flt-menu-" + currentSection)
-      .classList.remove("flt-menu-active");
-    currentSection -= 1;
-    document
-      .getElementById("flt-menu-" + currentSection)
-      .classList.add("flt-menu-active");
-  }
-  console.log(currentSlide);
-  console.log(sectionNums[currentSection - 1]);
-  console.log(currentSection);
-});
-tableButton.addEventListener("click", function () {
-  table.classList.toggle("flt-flex");
+//Listeners
+menuElements.forEach(function (elem, index) {
+  elem.addEventListener("click", function () {
+    changeSlide(sectionNums[index]);
+  });
 });
 tableElements.forEach(function (elem, index) {
   elem.addEventListener("click", function () {
-    changeSlide2(index);
-    if (currentSlide > 0) {
-      leftButton.classList.remove("flt-none");
-    } else {
-      leftButton.classList.add("flt-none");
-    }
-    if (currentSlide == slides.length) {
-      rightButton.classList.add("flt-none");
-    }
-    if (currentSlide < slides.length) {
-      rightButton.classList.remove("flt-none");
-    }
+    changeSlide(index);
   });
+});
+revertButton.addEventListener("click", function () {
+  changeSlide(0);
+});
+leftButton.addEventListener("click", function () {
+  changeSlide(currentSlide - 1);
+});
+rightButton.addEventListener("click", function () {
+  changeSlide(currentSlide + 1);
+});
+tableButton.addEventListener("click", function () {
+  table.classList.toggle("flt-flex");
 });
