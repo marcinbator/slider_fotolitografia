3ead710df163dd07ecda8fceddf3e6da66814703
Marcinek <marcinbator.ofc@gmail.com>
2023-03-12 20:43:34 +0100

    slide2

--- src/v_full/script.js
+++ src/v_full/script.js
@@ -37,28 +37,44 @@ const slides = [
       w warstwie SiO<sub>2</sub>.
     </div>
     <img class="flt-image" src="images/flt-1.png" alt="" />`,
-  `<h3>Etap II - Nakładanie emulsji światłoczułej na podłoże Si</h3>
-    <img class="flt-image" src="images/flt-1.png" alt="" />
-    <div class="flt-text">
-      Proces nakładania cienkiej warstwy
-      <strong>emulsji światłoczułej</strong> na powierzchnię utlenionej
-      płytki podłożowej. Bardzo ważne jest uzyskanie równomiernej warstwy
-      emulsji, dlatego płytka jest mocowana przyssawką próżniową do
-      stolika wykonującego szybki ruch obrotowy (ok. 20 000 obr/min).
-    </div>`,
+  `<h2>Podstawowe fazy fotolitografii</h2>
+          <h3>Etap II - Nakładanie emulsji światłoczułej na podłoże Si</h3>
+          <div class="flt-animation">
+            <div
+              class="flt-anim-up"
+              style="background-color: black; width: 50%; height: 10px"
+            >
+              <span class="flt-desc">&#8595 Warstwa światłoczuła (fotorezyst)</span>
+            </div>
+            <img class="flt-image" src="images/flt-1.png" alt="" />
+            <i
+              id="flt-play-1"
+              class="flt-play flt-button fa-regular fa-circle-play"
+              ><span class="flt-tip">Odtwórz</span></i
+            >
+          </div>
+          <div class="flt-text">
+            Proces nakładania cienkiej warstwy
+            <strong>emulsji światłoczułej</strong> na powierzchnię utlenionej
+            płytki podłożowej. Bardzo ważne jest uzyskanie równomiernej warstwy
+            emulsji, dlatego płytka jest mocowana przyssawką próżniową do
+            stolika wykonującego szybki ruch obrotowy (ok. 20 000 obr/min).
+          </div>`,
   "<h2>Slajd 3</h2>",
-  //2
   "<h2>Slajd 4</h2>",
   "<h2>Slajd 5</h2>",
   "<h2>Slajd 6</h2>",
-  //3
+  //2
   "<h2>Slajd 7</h2>",
   "<h2>Slajd 8</h2>",
   "<h2>Slajd 9</h2>",
-  //4
   "<h2>Slajd 10</h2>",
   "<h2>Slajd 11</h2>",
   "<h2>Slajd 12</h2>",
+  //4
+  "<h2>Slajd 13</h2>",
+  "<h2>Slajd 14</h2>",
+  "<h2>Slajd 15</h2>",
 ];
 const sections = [
   "Fotolitografia",
@@ -66,7 +82,7 @@ const sections = [
   "Domieszkowanie lokalne",
   "Nowinki techniczne",
 ];
-const sectionNums = [0, 3, 6, 9];
+const sectionNums = [0, 6, 11, 14];
 
 ////SLIDER
 //Buttons
@@ -76,7 +92,6 @@ const rightButton = document.getElementById("flt-right");
 const tableButton = document.getElementById("flt-table-button");
 const mobileMenuButton = document.getElementById("flt-top-menu-arrow");
 const fullScreen = document.getElementById("flt-full");
-const defButtons = document.querySelectorAll(".flt-def");
 
 //Elements
 const slide = document.getElementById("flt-slide");
@@ -84,7 +99,6 @@ const table = document.getElementById("flt-table");
 const tableElements = document.querySelectorAll(".flt-table-elem");
 const menuElements = document.querySelectorAll(".flt-side-elem");
 const slidesContainer = document.querySelector(".flt-slide-container");
-const defElems = document.querySelectorAll("flt-def-window");
 
 //Indicators
 var currentSlide = 0;
@@ -101,6 +115,7 @@ function fltChangeSlide(num) {
   console.log("FLTSlide: " + slideIndicator);
   fltCheckArrows();
   fltCheckSections();
+  fltLoadSlide();
 }
 function fltCheckArrows() {
   if (currentSlide > 0) {
@@ -121,7 +136,7 @@ function fltCheckSections() {
     var a = i + 1;
     if (
       (currentSlide >= sectionNums[i] && currentSlide < sectionNums[a]) ||
-      currentSlide >= 9
+      currentSlide >= sectionNums[2] + 1
     ) {
       document
         .getElementById("flt-menu-" + currentSection)
@@ -158,11 +173,7 @@ menuElements.forEach(function (elem, index) {
 tableElements.forEach(function (elem, index) {
   elem.addEventListener("click", function () {
     fltChangeSlide(index);
-  });
-});
-defButtons.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    elem.querySelector(".flt-def-window").classList.toggle("flt-block");
+    console.log("xd");
   });
 });
 revertButton.addEventListener("click", function () {
@@ -185,3 +196,39 @@ mobileMenuButton.addEventListener("click", function () {
 fullScreen.addEventListener("click", fltToggleFullScreen);
 
 ////CONTENT
+//Buttons
+var defButtons = document.querySelectorAll(".flt-def");
+var play1 = document.getElementById("flt-play-1");
+//Elements
+var defElems = document.querySelectorAll("flt-def-window");
+//Listeners
+defButtons.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    elem.querySelector(".flt-def-window").classList.toggle("flt-block");
+  });
+});
+play1?.addEventListener("click", function () {
+  fltSlideAnim1();
+});
+//Functions
+function fltSlideAnim1() {
+  element = document.querySelector(".flt-anim-up");
+  element.style.bottom = "50px";
+  setTimeout(function () {
+    element.style.bottom = "0px";
+    document.querySelector(".flt-desc").classList.add("flt-block");
+  }, 500);
+}
+function fltLoadSlide() {
+  defElems = document.querySelectorAll("flt-def-window");
+  defButtons = document.querySelectorAll(".flt-def");
+  play1 = document.getElementById("flt-play-1");
+  defButtons.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      elem.querySelector(".flt-def-window").classList.toggle("flt-block");
+    });
+  });
+  play1?.addEventListener("click", function () {
+    fltSlideAnim1();
+  });
+}
